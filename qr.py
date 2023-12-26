2490b3721fd64e7d1b86ef3ec5d70bbcb61a823f
netr <programmatical@gmail.com>
2023-12-26 11:53:26 -0800

    refactor qr code into a class

--- qr.py
+++ qr.py
@@ -1,9 +1,3 @@
-def build(version):
-    modules = get_module_size_from_version(version)
-    grid = [[0] * modules for _ in range(modules)]
-    return grid
-
-
 class InvalidVersionNumber(Exception):
     """Version number must be between 1 and 40
 
@@ -13,63 +7,86 @@ class InvalidVersionNumber(Exception):
     pass
 
 
-def get_module_size_from_version(version: int) -> int:
-    """
-    There are 40 versions of QR codes (from Version 1 (21 × 21 modules) to Version 40 (177 × 177 modules)).
-    Each version has a different module configuration for storing different amounts of data.
-    Each increment in version number increases the number of modules by 4 per side.
-    More: https://tritonstore.com.au/qr-code-size/
-    :param version: Version number between 1-40
-    :return: Number of modules to be represented by a nxn grid
-    """
-    if version <= 0 or version > 40:
-        raise InvalidVersionNumber(version)
+class QrCode:
+    MAX_VERSION = 40  # REF 1
+    MIN_VERSION = 1
+    MODULES_INCREMENT = 4
+    MIN_MODULES = 21
+    FINDER_OFFSET = 7
 
-    return 4 * (version - 1) + 21
+    def __init__(self, version: int):
+        self.version = version
+        self.modules = self.get_module_size()
+        self.matrix = [[0] * self.modules for _ in range(self.modules)]
+        self.add_patterns_and_separators()
 
+    def get_module_size(self) -> int:
+        """
+        There are 40 versions of QR codes (from Version 1 (21 × 21 modules) to Version 40 (177 × 177 modules)).
+        Each version has a different module configuration for storing different amounts of data.
+        Each increment in version number increases the number of modules by 4 per side.
+        More: https://tritonstore.com.au/qr-code-size/
+        :return: Number of modules to be represented by a nxn grid
+        """
+        if self.version < self.MIN_VERSION or self.version > self.MAX_VERSION:
+            raise InvalidVersionNumber(self.version)
 
-def add_finder_pattern(grid, xoffset, yoffset):
-    """
-    The top-left finder pattern's top left corner is always placed at (0,0).
-    The top-right finder pattern's top LEFT corner is always placed at ([(((V-1)*4)+21) - 7], 0)
-    The bottom-left finder pattern's top LEFT corner is always placed at (0,[(((V-1)*4)+21) - 7])
-    """
-    for i in range(yoffset + 0, yoffset + 7):
-        for j in range(xoffset + 0, xoffset + 7):
-            grid[i][j] = 2
+        return self.MODULES_INCREMENT * (self.version - 1) + self.MIN_MODULES  # REF 1
 
-    for i in range(yoffset + 1, yoffset + 6):
-        for j in range(xoffset + 1, xoffset + 6):
-            grid[i][j] = 1
+    def add_patterns_and_separators(self):
+        top_right = self.calculate_finder_position()
+        bottom_left = self.calculate_finder_position(is_bottom_left=True)
 
-    for i in range(yoffset + 2, yoffset + 5):
-        for j in range(xoffset + 2, xoffset + 5):
-            grid[i][j] = 2
+        self.add_finder_pattern(0, 0)
+        self.add_finder_pattern(*top_right)  # Use tuple unpacking
+        self.add_finder_pattern(*bottom_left)
+        self.add_separators()
 
+    def calculate_finder_position(self, is_bottom_left=False):
+        mod_minus_offset = self.modules - self.FINDER_OFFSET  # REF 1
+        return (0, mod_minus_offset) if is_bottom_left else (mod_minus_offset, 0)
 
-def add_separator(grid, xoffset, yoffset):
-    for i in range(0, 8):
-        grid[i][xoffset + 7] = 4
-        grid[yoffset + 7][i] = 3
+    def add_finder_pattern(self, xoffset, yoffset):
+        """
+        The top-left finder pattern's top left corner is always placed at (0,0).
+        The top-right finder pattern's top LEFT corner is always placed at ([(((V-1)*4)+21) - 7], 0)
+        The bottom-left finder pattern's top LEFT corner is always placed at (0,[(((V-1)*4)+21) - 7])
+        """
+        for i in range(yoffset + 0, yoffset + 7):
+            for j in range(xoffset + 0, xoffset + 7):
+                self.matrix[i][j] = 2
 
+        for i in range(yoffset + 1, yoffset + 6):
+            for j in range(xoffset + 1, xoffset + 6):
+                self.matrix[i][j] = 1
 
-def add_horiz_separators(grid):
-    for i in range(0, 8):
-        grid[7][i] = 1
-        grid[7][len(grid[0]) - i - 1] = 1
-        grid[len(grid) - 8][i] = 1
+        for i in range(yoffset + 2, yoffset + 5):
+            for j in range(xoffset + 2, xoffset + 5):
+                self.matrix[i][j] = 2
 
+    def add_separator(self, xoffset, yoffset):
+        for i in range(0, 8):
+            self.matrix[i][xoffset + 7] = 4
+            self.matrix[yoffset + 7][i] = 3
 
-def add_vertical_separators(grid):
-    for i in range(0, 8):
-        grid[i][7] = 1
-        grid[i][len(grid[0]) - 8] = 1
-        grid[len(grid[0]) - i - 1][7] = 1
+    def add_separators(self):
+        self.add_horiz_separators()
+        self.add_vertical_separators()
 
+    def add_horiz_separators(self):
+        for i in range(0, (self.FINDER_OFFSET + 1)):
+            self.matrix[self.FINDER_OFFSET][i] = 1
+            self.matrix[self.FINDER_OFFSET][len(self.matrix[0]) - i - 1] = 1
+            self.matrix[len(self.matrix) - (self.FINDER_OFFSET + 1)][i] = 1
 
-def calculate_top_right(version: int) -> [int, int]:
-    return [(((version - 1) * 4) + 21) - 7, 0]
+    def add_vertical_separators(self):
+        for i in range(0, (self.FINDER_OFFSET + 1)):
+            self.matrix[i][self.FINDER_OFFSET] = 1
+            self.matrix[i][len(self.matrix[0]) - (self.FINDER_OFFSET + 1)] = 1
+            self.matrix[len(self.matrix[0]) - i - 1][self.FINDER_OFFSET] = 1
 
+    def calculate_top_right(self) -> [int, int]:
+        return [(((self.version - 1) * 4) + self.MIN_MODULES) - self.FINDER_OFFSET, 0]
 
-def calculate_bottom_left(version: int) -> [int, int]:
-    return [0, (((version - 1) * 4) + 21) - 7]
+    def calculate_bottom_left(self) -> [int, int]:
+        return [0, (((self.version - 1) * 4) + self.MIN_MODULES) - self.FINDER_OFFSET]
--- test_qr.py
+++ test_qr.py
@@ -1,38 +1,53 @@
 import pytest
 
-from qr import get_module_size_from_version, InvalidVersionNumber, build, calculate_top_right, calculate_bottom_left, \
-    add_finder_pattern, add_horiz_separators, add_vertical_separators
+from qr import InvalidVersionNumber, QrCode
 
 
 def test_get_module_size_from_version():
-    assert get_module_size_from_version(1) == 21
-    assert get_module_size_from_version(40) == 177
+    assert QrCode(1).get_module_size() == 21
+    assert QrCode(40).get_module_size() == 177
 
     with pytest.raises(InvalidVersionNumber) as ex:
-        get_module_size_from_version(0)
+        QrCode(0).get_module_size()
 
 
-def test_build():
+def test_generate_base_matrix_with_finder_patterns_and_separators():
     version = 2
-    grid = build(version)
-    print(len(grid), len(grid[0]))
-    print(calculate_top_right(version))
-    top_right = calculate_top_right(version)
-    bottom_left = calculate_bottom_left(version)
-    add_finder_pattern(grid, 0, 0)
-    add_finder_pattern(grid, top_right[0], top_right[1])
-    add_finder_pattern(grid, bottom_left[0], bottom_left[1])
-    add_horiz_separators(grid)
-    add_vertical_separators(grid)
-    # add_separator(grid, xoffset=0, yoffset=8)
-    # add_separator(grid, bottom_left[0] + 7, bottom_left[1] - 7)
-    for r in grid:
-        print(r)
+    qr = QrCode(version)
+    grid = [[2, 2, 2, 2, 2, 2, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 2, 2, 2, 2, 2, 2],
+            [2, 1, 1, 1, 1, 1, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 1, 1, 1, 1, 1, 2],
+            [2, 1, 2, 2, 2, 1, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 1, 2, 2, 2, 1, 2],
+            [2, 1, 2, 2, 2, 1, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 1, 2, 2, 2, 1, 2],
+            [2, 1, 2, 2, 2, 1, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 1, 2, 2, 2, 1, 2],
+            [2, 1, 1, 1, 1, 1, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 1, 1, 1, 1, 1, 2],
+            [2, 2, 2, 2, 2, 2, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 2, 2, 2, 2, 2, 2],
+            [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [2, 2, 2, 2, 2, 2, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [2, 1, 1, 1, 1, 1, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [2, 1, 2, 2, 2, 1, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [2, 1, 2, 2, 2, 1, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [2, 1, 2, 2, 2, 1, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [2, 1, 1, 1, 1, 1, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [2, 2, 2, 2, 2, 2, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+
+    assert grid == qr.matrix
 
 
 def test_calculate_finder_pattern_positions():
-    assert calculate_top_right(1) == [14, 0]
-    assert calculate_bottom_left(1) == [0, 14]
+    qr = QrCode(1)
+    assert qr.calculate_top_right() == [14, 0]
+    assert qr.calculate_bottom_left() == [0, 14]
 
-    assert calculate_top_right(32) == [138, 0]
-    assert calculate_bottom_left(32) == [0, 138]
+    qr = QrCode(32)
+    assert qr.calculate_top_right() == [138, 0]
+    assert qr.calculate_bottom_left() == [0, 138]
